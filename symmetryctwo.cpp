778204d5b88324ff068b8d867959c563810f141f
daimonie <daimonie@Atlantis.Atlantis>
2015-12-22 14:46:38 +0100

    C2 should have twelves, others own't.

--- symmetryctwo.cpp
+++ symmetryctwo.cpp
@@ -3,13 +3,13 @@
 
 void symmetry_c2::bath()
 {   
-	bath_field[0][0] = 1; 
-	bath_field[0][4] = 1; 
-	bath_field[0][8] = 1; 
+	bath_field[0][0] = 12; 
+	bath_field[0][4] = 12; 
+	bath_field[0][8] = 12; 
 	 
-	bath_field[1][0] = -1; 
-	bath_field[1][4] = -1; 
-	bath_field[1][8] = 1;	 
+	bath_field[1][0] = -12; 
+	bath_field[1][4] = -12; 
+	bath_field[1][8] = 12;	 
 }
 string symmetry_c2::label()
 {
